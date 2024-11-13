cfa57db63c1ce5ed518aa8975547e794fac03b91
ksharief215 <ksharief377@boyslatin.net>
2024-11-13 14:35:24 +0000

    notes

---
+++ unit3_python/operateReview.py
@@ -0,0 +1,20 @@
+# Arithmatic operator - writing code that does math processes
+
+# an example of a computer program that uses arithmatic is a bank app
+depositAmount =50
+withdrawAmount = 25
+checkingAccount = 1000
+savingAccount = 2000
+CreditAmount = 1500
+
+print(depositAmount + checkingAccount)
+
+# Assingment operators- giving variables values, KEYWORD is assignment
+# any time you are using a variable, you are using the assingment operator
+
+x= 5
+x +=3
+
+# Comparison
+
+# Logical
---
+++ unit3_python/pythonFoundation.py
@@ -0,0 +1,36 @@
+# data type = basic building block of python programming.
+
+# string - data represented as characters wrapped.
+# together in quotation.
+
+# integer = whole numbers.
+678
+
+# floats = decimal numbers
+678.99
+
+#boolean = true or false values
+True
+False
+
+# variables = container for data
+# we create variables using the following steps:
+# 1. create variable name
+# 2. apply an equal sign
+# 3. give it a data type value
+
+user = "Khaseer"
+message = "Good morning"
+weather = "It is 60 degrees and sunny."
+# we use print to see data in our teminal
+print(message + user + "."+ weather)
+
+number1= 1200
+number2= 800
+
+print(number1, number2)
+
+name = 'My name is khaseer.'
+activity = 'I am going to do homework.'
+time='I will do this at 5:00.'
+print(name, activity, time)
