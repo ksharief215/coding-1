d5ce7fa32ae025876251cddc38465e67ada9335f
ksharief215 <ksharief377@boyslatin.net>
2024-11-18 14:11:35 +0000

    async activty november 15th

---
+++ unit3_python/asyncActivityNov11.py
@@ -0,0 +1,25 @@
+# ## Async Activity Novemeber 15th 2024 ##
+
+# 1.
+# we should use the logical operator because we need to compare 2
+# pieces of code
+name= 'Khaseer'
+passwords= '123'
+
+securityName= 'Khaseer'
+securityPassword= '123'
+
+print(name == securityName and password == securityPassword)
+
+#2. 
+ecgInStock= 10
+ecgShipment= 4
+print(ecgRequest < ecgRequest)
+oxygenRequest= 6
+oxygenShipment= 9
+
+print(oxygenRequest < oxygenShipment)
+
+#3
+msg=''
+msgReceivedBy=''
